38359e490df92c11855fd0b879cef7f418398c3a
Simon Baars <simon.mailadres@gmail.com>
2022-06-08 12:17:34 +0200

    Remove old product model

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -9,21 +9,7 @@ import {ProductsJson, RemoteProduct} from "./models/remote_product";
   providedIn: 'root'
 })
 export class ProductsService {
-
-  products: Product[] = [
-    {name: 'Banana', price: 123},
-    {name: 'Beanstalk', price: 233},
-    {name: 'Gum', price: 543},
-    {name: 'Apple', price: 553},
-    {name: 'Box', price: 8},
-    {name: 'Ammo', price: 64},
-    {name: 'Nerf Gun', price: 24},
-    {name: 'Keychain', price: 98},
-  ];
-
   remoteProducts: RemoteProduct[] = [];
-
-
   subject = new BehaviorSubject<RemoteProduct[]>([]);
 
   constructor(private httpClient: HttpClient) {
