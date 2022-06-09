8831ce1ca3d2ab471fc6bee220d45925706ba9cc
Simon Baars <simon.mailadres@gmail.com>
2022-06-09 12:45:38 +0200

    Product detail page finished

--- src/app/products/details.service.ts
+++ src/app/products/details.service.ts
@@ -7,23 +7,11 @@ import {HttpClient} from "@angular/common/http";
   providedIn: 'root'
 })
 export class DetailsService {
-  remoteProducts: ProductDetails[] = [];
-  subject = new BehaviorSubject<ProductDetails[]>([]);
-  filter = '';
 
   constructor(private httpClient: HttpClient) {}
 
-  fetchProducts(id: string): Observable<Product[]> {
+  fetchProductDetails(id: string): Observable<ProductDetails> {
     return this.httpClient
-      .get<ProductsJson>("https://s3-eu-west-1.amazonaws.com/developer-application-test/cart/"+id+"/details")
-      .pipe(map(x => x.products));
-  }
-
-  update(): void {
-    this.subject.next(this.remoteProducts.filter(e => e.name.toLowerCase().includes(this.filter.toLowerCase())));
-  }
-
-  get(): Observable<ProductDetails[]> {
-    return this.subject;
+      .get<ProductDetails>("https://s3-eu-west-1.amazonaws.com/developer-application-test/cart/"+id+"/detail");
   }
 }
--- src/app/products/product-details/product-details.component.ts
+++ src/app/products/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {ProductDetails} from "../../models/product";
+import {DetailsService} from "../details.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-product-details',
@@ -6,11 +9,12 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
-  @Input("id") id: string;
+  productDetails: ProductDetails = {product_id: "0", name: "", price: 0, image: "", description: ""};
 
-  constructor() { }
+  constructor(private detailsService: DetailsService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => this.detailsService.fetchProductDetails(params["id"]).subscribe(details => this.productDetails = details));
   }
 
 }
